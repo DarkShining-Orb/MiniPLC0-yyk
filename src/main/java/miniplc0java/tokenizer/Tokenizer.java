cafbb89f4a1b2b1270ef75a6a0dcd97c694a5ccd
DarkShining-Orb <269096320@qq.com>
2020-11-08 21:34:36 +0800

    a

--- src/main/java/miniplc0java/tokenizer/Tokenizer.java
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -44,9 +44,9 @@ public class Tokenizer {
     	String str = new String();
     	Pos startu = it.currentPos();
         // 直到查看下一个字符不是数字为止:
-    	while(Character.isDigit(it.peekchar())) {
+    	while(Character.isDigit(it.peekChar())) {
     		// -- 前进一个字符，并存储这个字符
-    		str = str + it.nextchar();
+    		str = str + it.nextChar();
     	}
         
         //
@@ -59,78 +59,65 @@ public class Tokenizer {
         // 解析成功则返回无符号整数类型的token，否则返回编译错误
         //
         // Token 的 Value 应填写数字的值
-        throw new Error("Not implemented");
+        //throw new Error("Not implemented");
     }
 
     private Token lexIdentOrKeyword() throws TokenizeError {
         // 请填空：
-    	String str;
+    	String str = new String();
     	Pos startu = it.currentPos();
     	
         // 直到查看下一个字符不是数字或字母为止:
-    	while(Character.isDigit(it.peekchar()) || Character.isAlphabetic(it.peekchar())) {
-    		str = str + it.nextchar(); // -- 前进一个字符，并存储这个字符
+    	while(Character.isDigit(it.peekChar()) || Character.isAlphabetic(it.peekChar())) {
+    		str = str + it.nextChar(); // -- 前进一个字符，并存储这个字符
     	}
         
         //
     	switch (str) {
     		case "begin":
-    			return new Token(Token.Begin, str, startu, it.currentPos());
-    			break;
+    			return new Token(TokenType.Begin, str, startu, it.currentPos());
     		case "end":
-    			return new Token(Token.End, str, startu, it.currentPos());
-    			break;
+    			return new Token(TokenType.End, str, startu, it.currentPos());
     		case "var":
-    			return new Token(Token.Var, str, startu, it.currentPos());
-    			break;
+    			return new Token(TokenType.Var, str, startu, it.currentPos());
     		case "const":
-    			return new Token(Token.Const, str, startu, it.currentPos());
-    			break;
+    			return new Token(TokenType.Const, str, startu, it.currentPos());
     		case "print":
-    			return new Token(Token.Print, str, startu, it.currentPos());
-    			break;
+    			return new Token(TokenType.Print, str, startu, it.currentPos());
     		default:
-    			return new Token(Token.Ident, str, startu, it.currentPos());
+    			return new Token(TokenType.Ident, str, startu, it.currentPos());
     	}
         // 尝试将存储的字符串解释为关键字
         // -- 如果是关键字，则返回关键字类型的 token
         // -- 否则，返回标识符
         //
         // Token 的 Value 应填写标识符或关键字的字符串
-        throw new Error("Not implemented");
+        //throw new Error("Not implemented");
     }
 
     private Token lexOperatorOrUnknown() throws TokenizeError {
         switch (it.nextChar()) {
             case '+':
                 return new Token(TokenType.Plus, '+', it.previousPos(), it.currentPos());
-                break;
             case '-':
                 // 填入返回语句
             	return new Token(TokenType.Minus, '-', it.previousPos(), it.currentPos());
                 //throw new Error("Not implemented");
-                break;
             case '*':
                 // 填入返回语句
             	return new Token(TokenType.Mult, '*', it.previousPos(), it.currentPos());
-                break;
             case '/':
                 // 填入返回语句
             	return new Token(TokenType.Div, '/', it.previousPos(), it.currentPos());
-                break;
             // 填入更多状态和返回语句
             case '=':
             	return new Token(TokenType.Equal, '=', it.previousPos(), it.currentPos());
-                break;
             case ';':
             	return new Token(TokenType.Semicolon, ';', it.previousPos(), it.currentPos());
-                break;
             case '(':
             	return new Token(TokenType.LParen, '(', it.previousPos(), it.currentPos());
-                break;
             case ')':
             	return new Token(TokenType.RParen, ')', it.previousPos(), it.currentPos());
-                break;
             default:
                 // 不认识这个输入，摸了
                 throw new TokenizeError(ErrorCode.InvalidInput, it.previousPos());
